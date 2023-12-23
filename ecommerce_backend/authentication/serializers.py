c8e0d3d4efba61c58306d4267ec0dd2f834fe1d6
SivanOp <sivanopenshtein@gmail.com>
2023-12-23 17:52:25 +0200

    starting the backend

--- ecommerce_backend/authentication/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from django.contrib.auth import get_user_model
-
-class CustomUserCreationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = get_user_model()
-        fields = ('custom_username', 'custom_password')  # Add other fields as needed
-        extra_kwargs = {'custom_password': {'write_only': True}}
