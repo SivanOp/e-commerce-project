f0950e62e365627919d651f6a9969577df27c606
SivanOp <sivanopenshtein@gmail.com>
2023-12-24 11:50:32 +0200

    http request

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Row, Col } from 'react-bootstrap'
 import products from '../products'
 import Product from '../components/Product'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 function HomeScreen() {
+  const[products, setProducts] = useState([])
+  useEffect(()=>{
+    async function fetchProduct()
+    {
+      const {data} = await axios.get('http://127.0.0.1:8000/api/products/')
+      setProducts(data)
+    }
+
+  },[])
+
+
   return (
     <div>
       <h1>Latest Products</h1>
